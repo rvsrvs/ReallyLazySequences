ef35594f37ecd4f4f9136463e50ef35402eb29fb
Van Simmons <rvsrvs@gmail.com>
2017-05-23 09:18:17 -0400

    Cleaned up composable

--- ReallyLazySequences/ReallyLazySequences/Composable.swift
+++ ReallyLazySequences/ReallyLazySequences/Composable.swift
@@ -9,38 +9,49 @@
 public protocol Composable {
     associatedtype OutputType
     associatedtype HeadType
-    func compose(_ output: @escaping (OutputType?) -> Any) -> ((HeadType?) -> Any)
-    func map<U>(_ transform: @escaping (OutputType) -> U ) -> Chained<Self, U>
+    func compose(_ output: @escaping (OutputType?) -> Continuation) -> ((HeadType?) -> Continuation)
+    func map<U>(_ transform: @escaping (OutputType) -> U ) -> Chain.Map<Self, U>
 }
 
 public extension Composable {
-    public func map<U>(_ transform: @escaping (OutputType) -> U ) -> Chained<Self, U> {
-        return Chained<Self, U>(predecessor: self) { (delivery: @escaping (U?) -> Any) -> ((OutputType?) -> Any) in
-            func accept(input: OutputType?) -> Any {
+    public func map<T>(_ transform: @escaping (OutputType) -> T ) -> Chain.Map<Self, T> {
+        return Chain.Map<Self, T>(predecessor: self) { (delivery: @escaping (T?) -> Continuation) -> ((OutputType?) -> Continuation) in
+            return { (input: OutputType?) -> Continuation in
                 guard let input = input else { return { delivery(nil) } }
                 return { delivery(transform(input)) }
             }
-            return accept
         }
     }
 }
 
-public struct Chained<Predecessor: Composable, Output> {
-    public typealias HeadType = Predecessor.HeadType
-    public typealias OutputType = Output
-    public typealias PredecessorType = Predecessor
-    
-    typealias Composer = (@escaping (OutputType?) -> Any) -> ((Predecessor.OutputType?) -> Any)
-    public var predecessor: Predecessor
-    var composer: Composer
-    
-    init(predecessor: Predecessor, composer: @escaping Composer) {
-        self.predecessor = predecessor
-        self.composer = composer
-    }
-    
-    public func compose(_ output: @escaping (Output?) -> Any) -> ((Predecessor.HeadType?) -> Any) {
+public protocol ChainedComposable: Composable {
+    associatedtype PredecessorType: Composable
+    typealias Composer = (@escaping (OutputType?) -> Continuation) -> ((PredecessorType.OutputType?) -> Continuation)
+    var predecessor: PredecessorType { get set }
+    var composer: Composer { get set }
+    init(predecessor: PredecessorType, composer: @escaping Composer)
+    func compose(_ output: @escaping (OutputType?) -> Continuation) -> ((PredecessorType.HeadType?) -> Continuation)
+}
+
+public extension ChainedComposable {
+    public func compose(_ output: @escaping (OutputType?) -> Continuation) -> ((PredecessorType.HeadType?) -> Continuation) {
         let composition = self.composer(output)
         return predecessor.compose(composition)
+    }
+}
+
+public struct Chain {
+    public struct Map<Predecessor: Composable, Output>: ChainedComposable {
+        public typealias PredecessorType = Predecessor
+        public typealias HeadType = Predecessor.HeadType
+        public typealias OutputType = Output
+        
+        public var predecessor: Predecessor
+        public var composer: Composer
+        
+        public init(predecessor: PredecessorType, composer: @escaping Composer) {
+            self.predecessor = predecessor
+            self.composer = composer
+        }
     }
 }
