6733d939da6831a23758a63775ff1145522ed4b2
Van Simmons <rvsrvs@gmail.com>
2018-07-31 08:48:20 -0400

    Simplify

--- ReallyLazySequences/ReallyLazySequences/Extensions.swift
+++ ReallyLazySequences/ReallyLazySequences/Extensions.swift
@@ -42,44 +42,33 @@ public extension ChainedSequence {
 // 2. It's own associated function which takes the predecessors output type and operates on it to produce its own output type
 // 3. a particular function which is specific to the action being taken
 // Number 3 is what is being passed in to the initializer of the specific type in each case
+
+func drive(_ continuation: Continuation) {
+    var next = continuation(); while let current = next as? Continuation { next = current() }
+}
+
 public extension ReallyLazySequenceProtocol {
     // Map sequential values of one type to a value of the same or different type
     public func map<T>(_ transform: @escaping (OutputType) -> T ) -> Map<Self, T> {
-        return Map<Self, T>(predecessor: self) { (delivery: @escaping (T?) -> Continuation) -> (OutputFunction) in
-            return { (input: OutputType?) -> Continuation in
-                guard let input = input else { return { delivery(nil) } }
-                return { delivery(transform(input)) }
-            }
+        return Map<Self, T>(predecessor: self) { delivery in
+            { input in input == nil ? { delivery(nil) } : { delivery(transform(input!)) } }
         }
     }
     
     // A generalized form of reduce which allows collection of values until a condition is reached
-    // Collected values are then forwarded as the collecting type to the successor
+    // Collected values are then forwarded as the collecting type to the successor when the until condition is met
+    // this function allows the initial value to be reset before receiving input of nil
     public func collect<T>(
         initialValue: @autoclosure @escaping () -> T,
         combine: @escaping (T, OutputType) -> T,
         until: @escaping (T, OutputType?) -> Bool
     ) -> Reduce<Self, T> {
-        return Reduce<Self, T>(predecessor: self) { (delivery: @escaping (T?) -> Continuation) -> (OutputFunction) in
-            var nextPartialValue: T?
-            var partialValue = initialValue()
-            return { (input: OutputType?) -> Continuation in
-                guard let input = input else {
-                    return {
-                        var next = delivery(partialValue)()
-                        while let current = next as? Continuation { next = current() }
-                        next = delivery(nil)()
-                        while let current = next as? Continuation { next = current() }
-                        return ContinuationDone
-                    }
-                }
-                if let newPartialValue = nextPartialValue { partialValue = newPartialValue }
-                partialValue = combine(partialValue, input)
-                if until(partialValue, input) {
-                    nextPartialValue = initialValue()
-                    return { delivery(partialValue) }
-                }
-                nextPartialValue = nil
+        return Reduce<Self, T>(predecessor: self) { delivery in
+            var partialValue = initialValue(), nextPartialValue: T?
+            return { input in
+                guard let input = input else { drive(delivery(partialValue)); return { delivery(nil) } }
+                partialValue = combine(nextPartialValue ?? partialValue, input); nextPartialValue = nil
+                if until(partialValue, input) { drive(delivery(partialValue)); nextPartialValue = initialValue() }
                 return ContinuationDone
             }
         }
@@ -91,40 +80,28 @@ public extension ReallyLazySequenceProtocol {
 
     // filter values that do not meet a specified condition
     func filter(_ filter: @escaping (OutputType) -> Bool ) -> Filter<Self, OutputType> {
-        return Filter<Self, OutputType>(predecessor: self) { (delivery: @escaping OutputFunction) -> (OutputFunction) in
-            return { (input: OutputType?) -> Continuation in
-                if input == nil || filter(input!) { return { delivery(input) } }
-                return ContinuationDone
-            }
+        return Filter<Self, OutputType>(predecessor: self) { delivery in
+            { input in (input == nil || filter(input!)) ? { delivery(input) } :  ContinuationDone }
         }
     }
     
     // create a sequence of values from a single value
     func flatMap<T>(_ transform: @escaping (OutputType) -> Producer<T>) -> FlatMap<Self, T> {
-        return FlatMap<Self, T>(predecessor: self) { (delivery: @escaping (T?) -> Continuation) -> (OutputFunction) in
-            return { (input: OutputType?) -> Continuation in
+        return FlatMap<Self, T>(predecessor: self) { delivery in
+            { input in
                 guard let input = input else { return { delivery(nil) } }
-                let producer = transform(input)
-                let task = producer.task { (value: T?) -> Continuation in
-                    guard let value = value else { return ContinuationDone }
-                    return delivery(value)
-                }
-                return { try? task.start(); return nil }
+                try? transform(input)
+                    .task { value in value != nil ? delivery(value) : ContinuationDone }
+                    .start()
+                return ContinuationDone
             }
         }
     }
 
     // Perform the rest of the push on another dispatch queue
     public func dispatch(_ queue: OperationQueue) -> Dispatch<Self, OutputType> {
-        return Dispatch<Self, OutputType>(predecessor: self) { (delivery: @escaping OutputFunction) -> (OutputFunction) in
-            return { (input: OutputType?) -> Continuation in
-                let op = BlockOperation {
-                    var next = delivery(input)()
-                    while let current = next as? Continuation { next = current() }
-                }
-                queue.addOperation(op)
-                return ContinuationDone
-            }
+        return Dispatch<Self, OutputType>(predecessor: self) { delivery in
+            { input in queue.addOperation(BlockOperation { drive(delivery(input)) } ); return ContinuationDone }
         }
     }
 }
