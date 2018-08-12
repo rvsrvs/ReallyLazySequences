0d1e775ac1922d00c53f7ce1f8e0a1a3d2238880
Van Simmons <rvsrvs@gmail.com>
2018-08-11 21:26:39 -0400

    Cleaning up consumer

--- ReallyLazySequences/ReallyLazySequences/Consumer.swift
+++ ReallyLazySequences/ReallyLazySequences/Consumer.swift
@@ -24,48 +24,20 @@ public protocol ConsumerProtocol {
     ) throws -> Void
 }
 
-public struct Consumer<Predecessor: ReallyLazySequenceProtocol>: ConsumerProtocol {
-    public typealias InputType = Predecessor.InputType
-    public let predecessor: Predecessor
-    // NB Predecessor.InputType is the type of the head of the sequence,
-    // NOT the specific output type for the Predecessor's Predecessor
-    private(set) public var composition: (Predecessor.InputType?) throws -> Void
-    
-    public init(predecessor:Predecessor, delivery: @escaping ((Predecessor.OutputType?) -> Void)) {
-        self.predecessor = predecessor
-        var isComplete = false
-        
-        let deliveryWrapper = { (value: Predecessor.OutputType?) -> Continuation in
-            delivery(value)
-            return ContinuationDone
-        }
-        // Have the predecessor compose its operation with ours
-        // Different types of predecessors compose differently
-        // This call eventually recurses through all predecessors
-        // terminating at an RLS structure.
-        let _composition = predecessor.compose(deliveryWrapper)
-        
-        // Consumer composes the final push function here.
-        composition = { value in
-            guard !isComplete else { throw ReallyLazySequenceError.isComplete }
-            if value == nil { isComplete = true }
-            try _composition(value)
-        }
-    }
-    
+public extension ConsumerProtocol {
     // Accept a push of the Head type and pass it through the composed closure
     //
-    public func push(_ value: Predecessor.InputType?) throws -> Void {
-        guard Predecessor.InputType.self != Void.self else { throw ReallyLazySequenceError.nonPushable }
+    public func push(_ value: InputType?) throws -> Void {
+        guard InputType.self != Void.self else { throw ReallyLazySequenceError.nonPushable }
         try composition(value)
     }
     
     // Accept a push of a closure which
     public func push(
         queue: OperationQueue? = nil,
-        _ producer: @escaping ((Predecessor.InputType?) throws-> Void) throws-> Void
+        _ producer: @escaping ((InputType?) throws-> Void) throws-> Void
         ) throws -> Void {
-        guard Predecessor.InputType.self != Void.self else { throw ReallyLazySequenceError.nonPushable }
+        guard InputType.self != Void.self else { throw ReallyLazySequenceError.nonPushable }
         if let queue = queue {
             queue.addOperation {
                 try? producer(self.composition)
@@ -76,6 +48,35 @@ public struct Consumer<Predecessor: ReallyLazySequenceProtocol>: ConsumerProtoco
             } catch {
                 throw error
             }
+        }
+    }
+}
+
+public struct Consumer<Predecessor: ReallyLazySequenceProtocol>: ConsumerProtocol {
+    public typealias InputType = Predecessor.InputType
+
+    // NB Predecessor.InputType is the type of the head of the sequence,
+    // NOT the specific output type for the Predecessor's Predecessor
+    private(set) public var composition: (Predecessor.InputType?) throws -> Void
+    
+    public init(predecessor:Predecessor, delivery: @escaping ((Predecessor.OutputType?) -> Void)) {
+        var isComplete = false
+        
+        let deliveryWrapper = { (value: Predecessor.OutputType?) -> Continuation in
+            delivery(value)
+            return ContinuationDone
+        }
+        // Have the predecessor compose its operation with ours
+        // Different types of predecessors compose differently
+        // This call eventually recurses through all predecessors
+        // terminating at an RLS structure.
+        let predecessorComposition = predecessor.compose(deliveryWrapper)
+        
+        // Consumer composes the final push function here.
+        composition = { value in
+            guard !isComplete else { throw ReallyLazySequenceError.isComplete }
+            if value == nil { isComplete = true }
+            try predecessorComposition(value)
         }
     }
 }
