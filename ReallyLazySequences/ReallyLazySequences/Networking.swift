31feb96d8ba8d3a48cca6c1d3a0918932d63ca87
Van Simmons <rvsrvs@gmail.com>
2018-08-22 14:29:37 -0400

    Simplifying

--- ReallyLazySequences/ReallyLazySequences/Networking.swift
+++ ReallyLazySequences/ReallyLazySequences/Networking.swift
@@ -10,27 +10,30 @@ import Foundation
 
 public typealias DataFetchValue = (data: Data?, response: URLResponse?, netError: Error?)
 
-fileprivate let URLDataMapper = SimpleSequence<DataFetchValue>()
-    .map { (result: DataFetchValue) -> Result<Data> in
-        guard let response = result.response as? HTTPURLResponse, result.netError == nil else {
-            return .failure(result.netError!)
+extension URLSession {
+    func dataTask(with url: URL, completionHandler: @escaping (Result<Data>) -> Void) -> URLSessionTask {
+        return dataTask(with: url) { (data: Data?, response: URLResponse?, netError: Error?) in
+            guard let response = response as? HTTPURLResponse, netError == nil else {
+                return completionHandler(.failure(netError!))
+            }
+            guard response.statusCode >= 200 && response.statusCode < 300 else {
+                let error = NSError(domain: "ReallyLazySequences.URLDataFetcher",
+                                    code: 1000,
+                                    userInfo: ["url" : response.url ?? "unknown", "response": response, "msg": "Invalid response"]
+                )
+                return completionHandler(.failure(error))
+            }
+            guard let data = data  else {
+                let error = NSError(domain: "ReallyLazySequences.URLDataFetcher",
+                                    code: 1001,
+                                    userInfo: ["url" : response.url ?? "unknown", "response": response, "msg": "Valid response but no data"]
+                )
+                return completionHandler(.failure(error))
+            }
+            return completionHandler(.success(data))
         }
-        guard response.statusCode >= 200 && response.statusCode < 300 else {
-            let error = NSError(domain: "ReallyLazySequences.URLDataFetcher",
-                                code: 1000,
-                                userInfo: ["url" : response.url ?? "unknown", "response": response, "msg": "Invalid response"]
-            )
-            return .failure(error)
-        }
-        guard let data = result.data  else {
-            let error = NSError(domain: "ReallyLazySequences.URLDataFetcher",
-                                code: 1001,
-                                userInfo: ["url" : response.url ?? "unknown", "response": response, "msg": "Valid response but no data"]
-            )
-            return .failure(error)
-        }
-        return .success(data)
     }
+}
 
 public struct URLDataFetcher: SubsequenceProtocol {
     public typealias InputType = (url: URL, session: URLSession)
@@ -43,19 +46,20 @@ public struct URLDataFetcher: SubsequenceProtocol {
     
     public init() {
         self.init { (fetch: InputType, delivery: @escaping ((OutputType?) -> Void)) -> Void in
-            let task = fetch.session.dataTask(with: fetch.url) {
-                let c = URLDataMapper.consume{ result in
-                    let deliveryWrapper = { (value: OutputType?) -> ContinuationResult in
-                        delivery(value)
-                        return .done
-                    }
-                    _ = ContinuationResult.complete(
-                        .afterThen(.more({ deliveryWrapper(result) }),
-                               .more({ deliveryWrapper(nil) }))
-                    )
+            let task = fetch.session.dataTask(with: fetch.url, completionHandler: { (result: Result<Data>) in
+                let c = SimpleSequence<Result<Data>>()
+                    .consume { result in
+                        let deliveryWrapper = { (value: OutputType?) -> ContinuationResult in
+                            delivery(value)
+                            return .done
+                        }
+                        _ = ContinuationResult.complete(
+                            .afterThen(.more({ deliveryWrapper(result) }),
+                                       .more({ deliveryWrapper(nil) }))
+                        )
                 }
-                _ = try? c.process(($0, $1, $2))
-            }
+                _ = try? c.process(result)
+            })
             task.resume()
             while task.state != .completed { RunLoop.current.run(until: Date(timeIntervalSinceNow: 0.1)) }
         }
@@ -64,9 +68,9 @@ public struct URLDataFetcher: SubsequenceProtocol {
 
 public final class URLDataGenerator: ListenableGeneratorProtocol {
     public typealias InputType = (URL, URLSession)
-    public typealias ListenableType = DataFetchValue
-    public typealias ListenableSequenceType = ListenableSequence<DataFetchValue, URLDataGenerator>
-    public var listeners = [UUID: Listener<DataFetchValue, URLDataGenerator>]()
+    public typealias ListenableType = Result<Data>
+    public typealias ListenableSequenceType = ListenableSequence<Result<Data>, URLDataGenerator>
+    public var listeners = [UUID: Listener<Result<Data>, URLDataGenerator>]()
     public var generator: (InputType, @escaping (ListenableType?) -> Void) -> Void
 
     public init(_ generator: @escaping (InputType, @escaping (ListenableType?) -> Void) -> Void) {
@@ -74,40 +78,17 @@ public final class URLDataGenerator: ListenableGeneratorProtocol {
     }
 
     convenience init() {
-        self.init { (params: (URL, URLSession), delivery: @escaping ((DataFetchValue?) -> Void)) -> Void in
-            params.1.dataTask(with: params.0) { delivery(($0, $1, $2)); delivery(nil) } .resume()
+        self.init { (params: (URL, URLSession), delivery: @escaping ((Result<Data>?) -> Void)) -> Void in
+            params.1.dataTask(with: params.0) { delivery($0); delivery(nil) } .resume()
         }
     }
 }
 
 extension URLDataGenerator {
-    public typealias DataListener = ListenableMap<ListenableSequence<DataFetchValue, URLDataGenerator>, Result<Data>>
-    public func dataListener() -> DataListener {
+    public func jsonListener<JSONType: Decodable>(
+        decodingType: JSONType.Type
+    ) -> ListenableMap<ListenableSequence<Result<Data>, URLDataGenerator>, Result<JSONType>> {
         return listener()
-            .map { (result: DataFetchValue) -> Result<Data> in
-                guard let response = result.response as? HTTPURLResponse, result.netError == nil else {
-                    return .failure(result.netError!)
-                }
-                guard response.statusCode >= 200 && response.statusCode < 300 else {
-                    let error = NSError(domain: "ReallyLazySequences.URLDataProducer",
-                                        code: 1000,
-                                        userInfo: ["response": response, "msg": "Invalid response"]
-                    )
-                    return .failure(error)
-                }
-                guard let data = result.data  else {
-                    let error = NSError(domain: "ReallyLazySequences.URLDataProducer",
-                                        code: 1001,
-                                        userInfo: ["response": response, "msg": "Valid response but no data"]
-                    )
-                    return .failure(error)
-                }
-                return .success(data)
-            }
-    }
-    
-    public func jsonListener<JSONType: Decodable>(decodingType: JSONType.Type) -> ListenableMap<DataListener, Result<JSONType>> {
-        return dataListener()
             .map { (fetchResult: Result<Data>) -> Result<JSONType> in
                 switch fetchResult {
                 case .success(let data):
